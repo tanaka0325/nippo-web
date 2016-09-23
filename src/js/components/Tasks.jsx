a54184dc0a6e8ef3ca3d3dfa0f8eab2ea3cd5f0e
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-23 18:55:44 +0900

    Update task view

--- src/js/components/Tasks.jsx
+++ src/js/components/Tasks.jsx
@@ -6,10 +6,25 @@ export default class Tasks extends React.Component {
   }
 
   render() {
+    const tasks = this.props.tasks.map((task) => {
+      return (
+        <tr key={task.id}>
+          <td>
+            <input type="checkbox" id={task.id} name={task.id} value={task.id} />
+            <label htmlFor={task.id}>{task.text}</label>
+          </td>
+        </tr>
+      );
+    });
+
     return (
       <div className="content">
         <h3>{this.props.label}</h3>
-        <span>{JSON.stringify(this.props.tasks)}</span>
+        <table className="table">
+          <tbody>
+            {tasks}
+          </tbody>
+        </table>
       </div>
     );
   }
