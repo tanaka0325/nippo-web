f982b260926819e81bf8acefefb50e7bbd2177dd
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-23 17:31:24 +0900

    Fix eslint error in HeaderComponent

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 
-const Header = () => {
-  return (
-    <header className="header">
-      <nav className="nav">
-      <div className="nav-left">
-      </div>
+const Header = () =>
+  <header className="header">
+    <nav className="nav">
+      <div className="nav-left" />
       <div className="nav-center">
         Hello Header Component.
       </div>
-      <div className="nav-right">
-      </div>
-      </nav>
-    </header>
-  );
-};
+      <div className="nav-right" />
+    </nav>
+  </header>;
 
 export default Header;
