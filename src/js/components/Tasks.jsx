0ac88c65188fb6d22ad2dd9924907c397aedd3f5
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-23 19:23:22 +0900

    Add proptypes in tasks component

--- src/js/components/Tasks.jsx
+++ src/js/components/Tasks.jsx
@@ -1,21 +1,15 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+
+import Task from './Task.jsx';
+
+const propTypes = {
+  label: PropTypes.string.isRequired,
+  tasks: PropTypes.array,
+};
 
 export default class Tasks extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    const tasks = this.props.tasks.map((task) => {
-      return (
-        <tr key={task.id}>
-          <td>
-            <input type="checkbox" id={task.id} name={task.id} value={task.id} />
-            <label htmlFor={task.id}>{task.text}</label>
-          </td>
-        </tr>
-      );
-    });
+    const tasks = this.props.tasks.map(task => <Task key={task.id} task={task} />);
 
     return (
       <div className="content">
@@ -29,3 +23,5 @@ export default class Tasks extends React.Component {
     );
   }
 }
+
+Tasks.propTypes = propTypes;
