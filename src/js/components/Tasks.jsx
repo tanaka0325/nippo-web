7f1b4d7d70cf364448ea2e829a7ad8c48080a692
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-23 20:27:54 +0900

    Change tasks div color by status

--- src/js/components/Tasks.jsx
+++ src/js/components/Tasks.jsx
@@ -11,15 +11,38 @@ export default class Tasks extends React.Component {
   render() {
     const tasks = this.props.tasks.map(task => <Task key={task.id} task={task} />);
 
+    const clx = () => {
+      switch (this.props.label) {
+        case 'TODO':
+          return 'is-danger';
+        case 'DOING':
+          return 'is-warning';
+        case 'DONE':
+          return 'is-success';
+        default:
+          return '';
+      }
+    };
+
     return (
-      <div className="content">
-        <h3>{this.props.label}</h3>
-        <table className="table">
-          <tbody>
-            {tasks}
-          </tbody>
-        </table>
-      </div>
+      <article className={`message ${clx()}`}>
+        <h3 className="message-header">{this.props.label}</h3>
+        <div className="message-body">
+          <table className="table">
+            <tbody>
+              {tasks}
+            </tbody>
+          </table>
+        </div>
+      </article>
+      // <div className="content">
+      //   <h3>{this.props.label}</h3>
+      //   <table className="table">
+      //     <tbody>
+      //       {tasks}
+      //     </tbody>
+      //   </table>
+      // </div>
     );
   }
 }
