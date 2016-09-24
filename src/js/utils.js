63302bba010b2e1ebf79304cee24e65be648ccc5
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-24 23:07:51 +0900

    Add formatDate function

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,4 +2,11 @@ export default class Utils {
   static paddingZero(num) {
     return `0${num}`.slice(-2);
   }
+
+  static formatDate(date) {
+    const y = date.getFullYear();
+    const m = this.paddingZero(date.getMonth() + 1);
+    const d = this.paddingZero(date.getDate());
+    return `${y}-${m}-${d}`;
+  }
 }
