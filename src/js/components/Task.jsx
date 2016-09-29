317515b850a46dc14decacfa04edaf5ee5a667c3
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-29 18:30:53 +0900

    Add icon in task, but not work yet

--- src/js/components/Task.jsx
+++ src/js/components/Task.jsx
@@ -11,6 +11,15 @@ export default class Task extends React.Component {
         <td>
           {this.props.task.text}
         </td>
+        <td className="is-icon">
+          <i className="fa fa-check" />
+        </td>
+        <td className="is-icon">
+          <i className="fa fa-pencil" />
+        </td>
+        <td className="is-icon">
+          <i className="fa fa-trash" />
+        </td>
       </tr>
     );
   }
