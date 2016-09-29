5e1db046eb6a9979d614240e48fb4980e7647fc8
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-09-29 22:54:29 +0900

    Hide done button if task statu is done

--- src/js/components/Task.jsx
+++ src/js/components/Task.jsx
@@ -23,14 +23,18 @@ export default class Task extends React.Component {
   }
 
   render() {
+    const buttonDone = (this.props.task.status !== 3) ?
+      <td className="is-icon" onClick={this.doneTask}>
+        <i className="fa fa-check" />
+      </td>
+      : <td />;
+
     return (
       <tr>
         <td>
           {this.props.task.text}
         </td>
-        <td className="is-icon" onClick={this.doneTask}>
-          <i className="fa fa-check" />
-        </td>
+        {buttonDone}
         <td className="is-icon">
           <i className="fa fa-pencil" />
         </td>
