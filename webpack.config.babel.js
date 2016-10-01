497f3131f17e093b9380e3d908393f8d173cfd21
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-10-01 23:47:40 +0900

    Prepare to write style environment

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,9 @@
 import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 module.exports = [
   {
+    name: 'js',
     entry: path.join(__dirname, 'src', 'js', 'index.jsx'),
     output: {
       path: path.join(__dirname, 'build'),
@@ -20,5 +22,26 @@ module.exports = [
     resolve: {
       extensions: ['.js', '.jsx'],
     },
+  },
+  {
+    name: 'style',
+    cache: true,
+    entry: path.join(__dirname, 'src', 'sass', 'style.sass'),
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'style.css',
+    },
+    target: 'web',
+    module: {
+      loaders: [
+        {
+          test: /\.sass$/,
+          loader: ExtractTextPlugin.extract('css?minimize!sass'),
+        },
+      ],
+    },
+    plugins: [
+      new ExtractTextPlugin('style.css'),
+    ],
   },
 ];
