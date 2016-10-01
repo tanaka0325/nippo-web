51f0d9744ce1865cb9ecf38b97eb34ae064fbd5f
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-10-01 23:48:14 +0900

    Update header style

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 
 const Header = () =>
-  <header className="header">
-    <nav className="nav">
-      <div className="nav-left" />
-      <div className="nav-center">
-        Hello Header Component.
-      </div>
-      <div className="nav-right" />
-    </nav>
-  </header>;
+  <section className="hero is-dark is-medium">
+    <div className="hero-head">
+      <header className="nav">
+        <div className="container">
+          <div className="nav-left">
+            <a className="nav-item site-title">
+              nippo
+            </a>
+          </div>
+          <div className="nav-right nav-menu">
+            <a className="nav-item">
+              <i className="fa fa-search" />
+              検索
+            </a>
+            <a className="nav-item">
+              <i className="fa fa-user-plus" />
+              登録
+            </a>
+            <a className="nav-item">
+              <i className="fa fa-sign-in" />
+              ログイン
+            </a>
+          </div>
+        </div>
+      </header>
+    </div>
+  </section>;
 
 export default Header;
