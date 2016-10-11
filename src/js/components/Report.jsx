34aaf38d63ee37773d3f3c7b0fe2cbe2be3dec33
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-10-11 21:41:30 +0900

    Add tab for switch view html and markdown

--- src/js/components/Report.jsx
+++ src/js/components/Report.jsx
@@ -16,10 +16,21 @@ class Report extends Component {
     super(props);
 
     this.editReport = this.editReport.bind(this);
+    this.changeTab = this.changeTab.bind(this);
+
+    this.state = {
+      activeTab: 'html',
+    };
   }
 
   editReport() {
     ReportActions.editReport(true);
+  }
+
+  changeTab(e) {
+    this.setState({
+      activeTab: e.target.name,
+    });
   }
 
   render() {
@@ -36,15 +47,33 @@ class Report extends Component {
             return '';
           },
         });
-        const result = md.render(this.props.report.body || '');
+        const htmlText = md.render(this.props.report.body || '');
+        const mdText = this.props.report.body;
+
+        const tabHtmlClx = (this.state.activeTab === 'html') ? 'is-active' : '';
+        const tabMdlClx = (this.state.activeTab === 'md') ? 'is-active' : '';
+
+        const renderBody = (this.state.activeTab === 'html') ? (
+          <p dangerouslySetInnerHTML={{ __html: htmlText }} />
+        ) : (
+          <p dangerouslySetInnerHTML={{ __html: mdText.replace(/\n/g, '<br/>') }} />
+        );
 
         return (
-          <div className="message-body content">
-            {/* <h1>{this.props.report.title}</h1> */}
-            <p dangerouslySetInnerHTML={{ __html: result }} />
-            <p className="control is-grouped is-grouped-centered">
-              <a className="button is-dark" onClick={this.editReport}>Edit</a>
-            </p>
+          <div className="message-body">
+            <div className="tabs is-small is-toggle is-fullwidth">
+              <ul>
+                <li className={tabHtmlClx}><a name="html" onClick={this.changeTab}>HTML</a></li>
+                <li className={tabMdlClx}><a name="md" onClick={this.changeTab}>Markdown</a></li>
+              </ul>
+            </div>
+            <div className="content">
+              {/* <h1>{this.props.report.title}</h1> */}
+              {renderBody}
+              <p className="control is-grouped is-grouped-centered">
+                <a className="button is-dark" onClick={this.editReport}>Edit</a>
+              </p>
+            </div>
           </div>
         );
       }
